5d277fd8394bfc04b1c654249370e6a518f34114
HiramZednem <hirammendez000@gmail.com>
2024-11-14 08:57:05 -0600

    funciona como dexe

--- routes/mineria.py
+++ routes/mineria.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 from typing import List, Dict, Union
@@ -16,18 +16,30 @@ class VistaResponse(BaseModel):
     vistas: int
     fecha: date
 
-@router.get("/vistas", response_model=List[VistaResponse])
-async def get_vistas_por_dia(db: AsyncSession = Depends(get_db)):
-    query = """
-    SELECT 
-      COUNT(*) AS vistas, 
-      DATE(created_at) AS fecha
-    FROM views
-    WHERE place_id = 199
-    GROUP BY DATE(created_at)
-    ORDER BY fecha;
+@router.get("/vistas/{place_id}", response_model=List[VistaResponse])
+async def get_vistas_por_dia(place_id: str, db: AsyncSession = Depends(get_db)):
+  
+  query = """
+    SELECT place_id 
+    FROM places 
+    WHERE google_id = :google_id;
     """
-    result = await db.execute(text(query))
-    rows = result.fetchall()
+  result = await db.execute(text(query), {"google_id": place_id})
+  place = result.fetchone()
+  
+  if not place:
+    raise HTTPException(status_code=404, detail="Error: place not found")
     
-    return [{"vistas": row.vistas, "fecha": row.fecha} for row in rows]
+  query = """
+  SELECT 
+    COUNT(*) AS vistas, 
+    DATE(created_at) AS fecha
+  FROM views
+  WHERE place_id = :place_id
+  GROUP BY DATE(created_at)
+  ORDER BY fecha;
+  """
+  result = await db.execute(text(query), {"place_id": place.place_id})
+  rows = result.fetchall()
+  
+  return [{"vistas": row.vistas, "fecha": row.fecha} for row in rows]
