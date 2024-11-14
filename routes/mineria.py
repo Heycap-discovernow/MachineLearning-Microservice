2684306c23506b314d363a7b074b5fa5b9fd7225
HiramZednem <hirammendez000@gmail.com>
2024-11-14 17:50:03 -0600

    jala pero prophet es muy pesado" cl ; / exit

--- routes/mineria.py
+++ routes/mineria.py
@@ -5,6 +5,8 @@ from typing import List, Dict, Union
 from routes.database import get_db
 from datetime import date
 from pydantic import BaseModel
+import pandas as pd
+from prophet import Prophet
 
 router = APIRouter()
 
@@ -41,5 +43,33 @@ async def get_vistas_por_dia(place_id: str, db: AsyncSession = Depends(get_db)):
   """
   result = await db.execute(text(query), {"place_id": place.place_id})
   rows = result.fetchall()
+  
+  '''
+  {
+        "vistas": 21,
+        "fecha": "2024-11-10"
+    },
+    {
+        "vistas": 9,
+        "fecha": "2024-11-11"
+    },
+  '''
+  df = pd.DataFrame(rows)
+  df.info()
+  df['fecha'] = pd.to_datetime(df['fecha'])
+  df.set_index('fecha', inplace=True)
+  
+  df_prophet = df.interpolate(method='time').copy()
+  df_prophet.reset_index(inplace=True)
+  df_prophet.rename(columns={'fecha': 'ds', 'vistas': 'y'}, inplace=True)
+
+  model = Prophet(daily_seasonality=True)
+  model.fit(df_prophet)
+  future = model.make_future_dataframe(periods=3)
+  forecast = model.predict(future)
+  predicted_data = forecast[forecast['ds'] > df_prophet['ds'].max()]
+   
+  # for _, row in predicted_data.iterrows():
+  #   rows.append({"vistas": int(row['yhat']), "fecha": row['ds'].date()})
   
   return [{"vistas": row.vistas, "fecha": row.fecha} for row in rows]
